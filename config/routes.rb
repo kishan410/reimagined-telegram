e63d3a514180f70060007665d7c8a08a040e5d46
Kishan <kishanpatel04@ymail.com>
2017-01-25 21:01:24 +0000

    Add a Static Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  get 'static_pages/homehelp'
+
+  get 'static_pages/about'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get ..." do
-    get static_pages_..._url
+  test "should get homehelp" do
+    get static_pages_homehelp_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
